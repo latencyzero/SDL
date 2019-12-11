665c41b8b91ce5267a00cff8c82f368aa80129d9
Rick Mann <rmann@latencyzero.com>
2019-12-11 03:43:11 -0800

    stuff

--- Sources/SDL/Text.swift
+++ Sources/SDL/Text.swift
@@ -23,10 +23,51 @@ SDLFont
 		self.internalPointer = try ip.sdlThrow(type: type(of: self))
 	}
 	
+	public
+	init(path inPath: String, size inSize: Int, index inIndex: Int)
+		throws
+	{
+		let ip = TTF_OpenFontIndex(inPath, Int32(inSize), inIndex)
+		self.internalPointer = try ip.sdlThrow(type: type(of: self))
+	}
+	
 	deinit
 	{
 		TTF_CloseFont(self.internalPointer)
 	}
 	
+	public
+	var
+	familyName: String
+	{
+		get
+		{
+			let v = TTF_FontFaceFamilyName(self.internalPointer)
+			return String(cString: v!)
+		}
+	}
+	
+	public
+	var
+	styleName: String
+	{
+		get
+		{
+			let v = TTF_FontFaceStyleName(self.internalPointer)
+			return String(cString: v!)
+		}
+	}
+	
+	public
+	var
+	faceCount: Int
+	{
+		get
+		{
+			return TTF_FontFaces(self.internalPointer)
+		}
+	}
+	
+
 	internal let internalPointer: OpaquePointer
 }
